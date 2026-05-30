14ef8a47dc1a14321f58d0d380783dabc956f1fc
segabass65 <segabass65@proton.me>
2026-05-30 23:58:12 +0300

    feat: add Makefile

---
+++ Makefile
@@ -0,0 +1,5 @@
+ASSETS_DIR = assets
+
+
+preview:
+	@catwalk latte.webp frappe.webp macchiato.webp mocha.webp -C $(ASSETS_DIR)
--- shell.nix
+++ shell.nix
@@ -1,3 +1,6 @@
 { pkgs ? import <nixpkgs> {} }: pkgs.mkShell {
-  nativeBuildInputs = [ pkgs.catppuccin-catwalk ];
+  nativeBuildInputs = with pkgs; [
+    catppuccin-catwalk
+    gnumake
+  ];
 }
